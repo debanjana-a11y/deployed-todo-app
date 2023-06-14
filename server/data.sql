784f6bbfb6e9684e12d1741cb7694688643cfedb
debanjana-a11y <89571066+debanjana-a11y@users.noreply.github.com>
2023-06-14 16:45:11 +0530

    Adds database and server running commands

---
+++ server/data.sql
@@ -0,0 +1,14 @@
+CREATE DATABSE todoapp;
+
+CREATE TABLE todos (
+    id VARCHAR(255) primary key,
+    user_email VARCHAR(50),
+    title VARCHAR(30),
+    progress INT,
+    date VARCHAR(300)    
+);
+
+CREATE TABLE users (
+    email VARCHAR(50) primary key,
+    hashed_password VARCHAR(255)
+);
