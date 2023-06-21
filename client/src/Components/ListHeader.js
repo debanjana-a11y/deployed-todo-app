9955040e56c0ca12eb73146d5b22289871a59c66
debanjana-a11y <89571066+debanjana-a11y@users.noreply.github.com>
2023-06-21 09:51:58 +0530

    Creates a modal for ADD and EDIT

--- client/src/Components/ListHeader.js
+++ client/src/Components/ListHeader.js
@@ -1,14 +1,24 @@
+import Modal from './Modal';
+
 function ListHeader({listName}) {
+  const create = () => {
+    // return (
+    //   <Modal/>
+    // );
+    return;
+  };
+
   const signout = () => {
     console.log("sign out");
-  }
+  };
 
   return (
     <div className="list-header">
       <h1>{listName}</h1>
       <div className="button-container">
-        <button className="create">ADD NEW</button>
+        <button className="create" onClick={create}>ADD NEW</button>
         <button className="signout" onClick={signout}>SIGN OUT</button>
+        <Modal mode={'edit'}/>
       </div>
     </div>
   );
--- client/src/Components/Modal.js
+++ client/src/Components/Modal.js
@@ -1,6 +1,38 @@
-function Modal() {
+function Modal({mode}) {
+    const handleChange = (e) => {
+      console.log("changing");
+    };
     return (
-      <div>Debanjana
+      <div className="overlay">
+        <div className ="modal">
+          <div className="form-title-container">
+            <h3>Let's {mode} your task</h3>
+            <button>X</button>
+          </div>
+
+          <form>
+            <input
+              required
+              name="title"
+              values={""}
+              placeholder={"Your Task goes here"}
+              maxLength={30}
+              onChange={handleChange}
+            />
+            <br/>
+            <label htmlFor="range">Drag to set your current progress</label>
+            <input
+              name="progress"
+              id="range"
+              type="range"
+              values={""}
+              min="0"
+              max="100"
+              onChange={handleChange}
+            />
+            <input type="submit" className={mode}/>
+          </form>
+        </div>
       </div>
     );
   }
